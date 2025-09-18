f96dd344acccb4cd3b52e28731757d7084693ed2
itashi00 <ash.ito.cms@gmail.com>
2025-09-18 17:00:50 +0900

    Update CarExec_Chapter15.java

--- kadai_015/CarExec_Chapter15.java
+++ kadai_015/CarExec_Chapter15.java
@@ -3,8 +3,7 @@ package kadai_015;
 public class CarExec_Chapter15 {
     public static void main(String[] args) {
         Car_Chapter15 car = new Car_Chapter15();
-        car.changeGear(3); // ギアを3に切り替え
-        car.run();         // 速度を表示
+        car.changeGear(3); // ギアを3に切り替え（出力は「ギア1から3に切り替えました」）
+        car.run();         // 速度を表示（「速度は時速30kmです」）
     }
 }
-
