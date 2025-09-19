93603c156e4b9be8b8c3ec132e562b153fd5fe34
itashi00 <ash.ito.cms@gmail.com>
2025-09-19 14:34:07 +0900

    Update DictionaryExec_Chapter21.java

--- kadai_021/DictionaryExec_Chapter21.java
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -1,36 +1,14 @@
 package kadai_021;
 
-import java.util.HashMap;
+public class DictionaryExec_Chapter21 {
+    public static void main(String[] args) {
+        // 辞書クラスのインスタンスを作成
+        Dictionary_Chapter21 dict = new Dictionary_Chapter21();
 
-public class Dictionary_Chapter21 {
-    // 辞書(HashMap)の宣言
-    private HashMap<String, String> dictionary = new HashMap<>();
+        // 調べる英単語を配列にセット
+        String[] words = {"apple", "banana", "grape", "orange"};
 
-    // コンストラクタで辞書に英単語と意味を追加
-    public Dictionary_Chapter21() {
-        dictionary.put("apple", "りんご");
-        dictionary.put("peach", "桃");
-        dictionary.put("banana", "バナナ");
-        dictionary.put("lemon", "レモン");
-        dictionary.put("pear", "梨");
-        dictionary.put("kiwi", "キウィ");
-        dictionary.put("strawberry", "いちご");
-        dictionary.put("grape", "ぶどう");
-        dictionary.put("muscat", "マスカット");
-        dictionary.put("cherry", "さくらんぼ");
-    }
-
-    // 辞書を調べるメソッド
-    public void searchWords(String[] words) {
-        for (String word : words) {
-            if (dictionary.containsKey(word)) {
-                // 辞書に存在する場合
-                System.out.println(word + " の意味は " + dictionary.get(word) + " です。");
-            } else {
-                // 辞書に存在しない場合
-                System.out.println(word + " は辞書に存在しません。");
-            }
-        }
+        // メソッドを呼び出して検索
+        dict.searchWords(words);
     }
 }
-
