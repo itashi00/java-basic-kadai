73fd1250687dda4d939e7a869f67ca1e2875118b
itashi00 <ash.ito.cms@gmail.com>
2025-09-19 15:36:01 +0900

    Update JyankenExec_Chapter28.java

--- kadai_028/JyankenExec_Chapter28.java
+++ kadai_028/JyankenExec_Chapter28.java
@@ -1,1 +1,18 @@
+package kadai_028;
+
+public class JyankenExec_Chapter28 {
+
+    public static void main(String[] args) {
+        Jyanken_Chapter28 jyanken = new Jyanken_Chapter28();
+
+        // 自分の手を入力
+        String myChoice = jyanken.getMyChoice();
+
+        // 相手の手を乱数で選ぶ
+        String opponentChoice = jyanken.getRandom();
+
+        // じゃんけん実行
+        jyanken.playGame(myChoice, opponentChoice);
+    }
+}
 
